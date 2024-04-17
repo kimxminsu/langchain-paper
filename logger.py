a94e9bd16f1a6b8f325347a4766624c409acf148
choi <zuzizzuziz1@gmail.com>
2024-04-17 17:56:48 +0900

    수정logger

--- logger.py
+++ logger.py
@@ -1,10 +1,11 @@
 # 시스템 활동 기록 처리
 from datetime import datetime
 class Logger:
-    def __init__(self):
+
+
+    def log(self, input, message):
         now = datetime.now()
         self.time = now.strftime("%D %H:%M:%S")
-    def log(self, input, message):
         try:
             with open('LangchainLog.txt', 'a') as file:
                 file.write(f'[{self.time}] Q:{input} \n A:{message}\n\n')
