a8ce9f581654a5e8c35cdb1d29b38cf1ba131af4
kimxminsu <kimxminsu@gmail.com>
2024-04-18 09:01:57 +0000

    refactor: Modify retriever
    - 테스트용 print 삭제

--- retriever.py
+++ retriever.py
@@ -6,5 +6,4 @@ class Retriever:
     def retrieve(self, vector_db):
         """벡터DB에서 데이터 가져와서 찾는 내용과 가장 가까운 데이터 반환"""
         knowledge = vector_db.as_retriever(k=2)
-        print(f"retriever.py-knowledge: {knowledge}")
         return knowledge
